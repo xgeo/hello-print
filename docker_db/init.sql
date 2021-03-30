5dc017c2141b1ce233b1916ef707657f7179a9d7
Geovanny Lino Coutinho <geovannylc@gmail.com>
2021-03-30 03:49:40 -0300

    #InitDB

--- docker_db/init.sql
+++ docker_db/init.sql
@@ -2,9 +2,9 @@ DROP DATABASE IF EXISTS hello_print;
 
 CREATE DATABASE hello_print;
 
-CREATE TABLE `requests` (
+CREATE TABLE requests (
    id bigserial PRIMARY KEY,
-   message VARCHAR(125) NOT NULL ,
+   message VARCHAR(125) NOT NULL,
    created_at TIMESTAMP NOT NULL,
-   updated_at TIMESTAMP NULL
+   updated_at TIMESTAMP
 );
